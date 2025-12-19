ed1803a30fed79101e16835382f1260da30002cc
Yinbaiyuan <yinbaiyuan@gmail.com>
2025-12-19 13:00:30 +0800

    ##调整指示灯显示效果

--- main/led/single_led.cc
+++ main/led/single_led.cc
@@ -4,9 +4,9 @@
 
 #define TAG "SingleLed"
 
-#define DEFAULT_BRIGHTNESS 4
-#define HIGH_BRIGHTNESS 16
-#define LOW_BRIGHTNESS 2
+#define DEFAULT_BRIGHTNESS 16
+#define HIGH_BRIGHTNESS 32
+#define LOW_BRIGHTNESS 8
 
 #define BLINK_INFINITE -1
 
